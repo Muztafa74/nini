0bd18eb7d8dd7e4306d09c44b40317607d689a48
muztafa74 <elqalfat@gmail.com>
2025-05-26 04:51:03 +0300

    Your descriptive commit message37

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
   },
   trailingSlash: true,
   distDir: 'out',
+  // Ensure CSS is properly handled
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
